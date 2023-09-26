337c3bed3a153f0275c95ea8574672c14e62a86e
Nastia Ohal <nastiaohal@Nastias-MacBook-Pro.local>
2023-09-26 16:56:38 -0500

    first commit

---
+++ Python/ex1.py
@@ -0,0 +1,16 @@
+cow = 35
+duck = 40
+chicken = 23
+
+chicken_legs= 2
+duck_legs=2
+cow_legs=4
+
+total_cow_legs = cow * cow_legs
+total_duck_legs = duck * duck_legs
+total_chicken_legs = chicken * chicken_legs
+
+result=total_cow_legs + total_chicken_legs + total_duck_legs
+
+print(result)
+print("We have legs", result, "in the farm")
---
+++ Python/ex4_remainder.py
@@ -0,0 +1,6 @@
+total_devops = 20
+number_of_scrum_teams = 8
+
+remainder_of_devops = 20 % 8
+
+print(remainder_of_devops)
